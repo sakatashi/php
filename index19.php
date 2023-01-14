9262dbb2183a9a4bcacf898b3ceb8d29b28988bb
sakatashi <shiori.1002sakata@gmail.com>
2023-01-14 10:47:41 +0000

    [Add]戻り値

---
+++ index19.php
@@ -0,0 +1,14 @@
+<?php
+
+  // 関数getCircleAreaを定義してください
+  function getCircleArea($radius) {
+    return $radius * $radius * 3;
+  }
+  
+  // 関数getCircleAreaを呼び出して、戻り値を変数$circleAreaに代入してください
+  $circleArea = getCircleArea(5);
+  
+  // $circleAreaをechoしてください
+  echo $circleArea;
+  
+?>
