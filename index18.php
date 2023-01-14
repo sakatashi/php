bb762f56e58a1ecc9789d7479c5763dc0bdc0c92
sakatashi <shiori.1002sakata@gmail.com>
2023-01-14 10:45:10 +0000

    [Add]関数自作

---
+++ index18.php
@@ -0,0 +1,20 @@
+<?php
+
+  // 関数helloを定義してください
+  function hello() {
+    echo 'Hello, world!';
+  }
+  
+  // 関数helloを呼び出してください
+  hello();
+  
+  echo '<br>';
+  // 関数printRectangleAreaを定義してください
+  function printRectangleArea($height, $width) {
+    echo $height * $width;
+  }
+  
+  // 引数を(5, 10)としてprintRectangleAreaを呼び出してください
+  printRectangleArea(5, 10);
+    
+?>
